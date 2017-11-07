8a7fae0f90e875cdc1b6631dbeb74f83b074644b
Peter Monks <pmonks@gmail.com>
2017-11-06 16:12:52 -0800

    :art: Tweak copyright date

--- src/bot_clj/eval.clj
+++ src/bot_clj/eval.clj
@@ -1,5 +1,5 @@
 ;
-; Copyright © 2016, 2017 Symphony Software Foundation
+; Copyright © 2017 Symphony Software Foundation
 ; SPDX-License-Identifier: Apache-2.0
 ;
 ; Licensed under the Apache License, Version 2.0 (the "License");
